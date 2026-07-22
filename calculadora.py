9d96034f670ca247664fc522113c09a6553e6e14
raycor952-tech <raycor952@gmail.com>
2026-07-22 15:22:08 -0500

    Update calculadora.py
    AGREGAR DIVISION
    RAYNER CORY

--- calculadora.py
+++ calculadora.py
@@ -1,4 +1,4 @@
-# CALCULADORA - VERSIÓN 3
+# CALCULADORA - VERSIÓN 4
 # Autor(a): RAYNER CORY
 # =============================================
 
@@ -7,11 +7,12 @@ print("CALCULADORA")
 num1 = float(input("Ingrese el primer número: "))
 num2 = float(input("Ingrese el segundo número: "))
 
-print("\ n1. Sumar")
+print("\n 1. Sumar")
 print(" 2. Restar")
 print(" 3. Multiplicar")
+print(" 4. Dividir")
 
-opcion = input("\n Seleccione una opción: ")
+opcion = input("\nSeleccione una opción: ")
 
 if opcion == "1":
     print("\n La suma es:", num1 + num2)
@@ -21,6 +22,12 @@ elif opcion == "2":
 
 elif opcion == "3":
     print("\n La multiplicación es:", num1 * num2)
+
+elif opcion == "4":
+    if num2 != 0:
+        print("\n La división es:", num1 / num2)
+    else:
+        print("No se puede dividir entre cero.")
 
 else:
     print("\n Opción no válida.")
