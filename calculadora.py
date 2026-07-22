9767a759b547058b0eddbb15f3af6acfc08d852b
raycor952-tech <raycor952@gmail.com>
2026-07-22 15:18:30 -0500

    Update calculadora.py
    AGREGAR LA RESTA
    RAYNER CORY

--- calculadora.py
+++ calculadora.py
@@ -1,9 +1,22 @@
-## CALCULADORA - VERSIÓN 1
-# Autor(a): NOMBRE APELLIDO
-# ===========================================
+# CALCULADORA - VERSIÓN 2
+# Autor(a): RAYNER CORY
+# =============================================
+
+print("CALCULADORA")
+
 num1 = float(input("Ingrese el primer número: "))
 num2 = float(input("Ingrese el segundo número: "))
 
-suma = num1 + num2
+print("\n1. Sumar")
+print("2. Restar")
 
-print("Resultado:", suma)
+opcion = input("\nSeleccione una opción: ")
+
+if opcion == "1":
+    print("\n La suma es:", num1 + num2)
+
+elif opcion == "2":
+    print("\n La resta es:", num1 - num2)
+
+else:
+    print("\n Opción no válida.")
